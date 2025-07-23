30a49fa46d920d5f567174c0ffa15bb5111e86bf
Septiarosalia <septiarosaliaocha@gmail.com>
2025-07-23 21:32:46 +0700

    update desain icon

--- admin/stats.php
+++ admin/stats.php
@@ -193,9 +193,7 @@ if ($result) {
                         <div class="stats-card">
                             <div class="d-flex align-items-center mb-3">
                                 <div class="stats-icon bg-warning text-white rounded-3 p-2 me-3">
-                                    <svg width="16" height="16" fill="white" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"/>
-                                    </svg>
+                                    <i class="bi bi-hourglass-split"></i>
                                 </div>
                                 <div>
                                     <h6 class="mb-0 text-muted">In Progress</h6>
