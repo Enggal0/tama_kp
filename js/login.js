92cf6f91f80843bec7e2036baa9c3a6417111910
Septiarosalia <septiarosaliaocha@gmail.com>
2025-07-31 19:48:32 +0700

    delete comments

--- js/login.js
+++ js/login.js
@@ -1,5 +1,4 @@
 document.getElementById('loginForm').addEventListener('submit', function (e) {
-  // e.preventDefault();
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
