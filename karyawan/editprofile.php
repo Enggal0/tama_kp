77fa2d2f3014b0abf0b5a238e69cc2262db36c56
Septiarosalia <septiarosaliaocha@gmail.com>
2025-08-29 21:01:46 +0700

    update icon

--- karyawan/editprofile.php
+++ karyawan/editprofile.php
@@ -170,9 +170,7 @@ if ($user['name']) {
                     <?php endif; ?>
 
                     <div class="alert alert-info">
-                        <svg width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"/>
-                        </svg>
+                        <i class="bi bi-info-circle fs-5"></i>
                         <div>
                             Please double check all the data you have entered to ensure its accuracy before proceeding to the next step.
                         </div>
@@ -181,9 +179,7 @@ if ($user['name']) {
                     <form class="edit-form" method="POST" enctype="multipart/form-data">
                         <div class="form-section">
                             <h2 class="section-title">
-                                <svg width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd"/>
-                                </svg>
+                                <i class="bi bi-card-image fs-5"></i>
                                 Profile Photo
                             </h2>
                             <div class="photo-upload-section">
@@ -204,9 +200,7 @@ if ($user['name']) {
 
                         <div class="form-section">
                             <h2 class="section-title">
-                                <svg width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"/>
-                                </svg>
+                                <i class="bi bi-person-circle fs-5"></i>
                                 Personal Information
                             </h2>
                             <div class="form-grid">
@@ -232,15 +226,13 @@ if ($user['name']) {
                         <!-- Additional Information -->
                         <div class="form-section">
                             <h2 class="section-title">
-                                <svg width="20" height="20" fill="currentColor" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" clip-rule="evenodd"/>
-                                </svg>
+                                <i class="bi bi-person-circle fs-5"></i>
                                 Additional Information
                             </h2>
                             <div class="form-grid">
                                 <div class="form-group">
                                     <label class="form-label">Gender</label>
-                                    <select class="form-input" name="gender" id="gender">
+                                    <select class="form-input" name="gender" id="gender" style="background-position: right 0.3rem center;">
                                         <option value="">Select Gender</option>
                                         <option value="male" <?= $user['gender'] === 'male' ? 'selected' : '' ?>>Male</option>
                                         <option value="female" <?= $user['gender'] === 'female' ? 'selected' : '' ?>>Female</option>
