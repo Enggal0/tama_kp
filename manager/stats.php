9955af3fbe8e14f48dbc48836ea1764013e43754
Septiarosalia <septiarosaliaocha@gmail.com>
2025-08-29 19:58:10 +0700

    update icon

--- manager/stats.php
+++ manager/stats.php
@@ -100,45 +100,35 @@ if ($result) {
             <div class="sidebar-nav">
                 <div class="nav-item">
                     <a href="dashboard.php" class="nav-link">
-                        <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"/>
-                        </svg>
+                        <i class="bi bi-grid-1x2-fill nav-icon"></i>
                         <span class="nav-text">Dashboard</span>
                     </a>
                 </div>
 
                 <div class="nav-item">
                     <a href="manageaccount.php" class="nav-link">
-                        <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"/>
-                        </svg>
+                        <i class="bi bi-people-fill nav-icon"></i>
                         <span class="nav-text">Account Overview</span>
                     </a>
                 </div>
 
                 <div class="nav-item">
                     <a href="managetask.php" class="nav-link">
-                        <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1 1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"/>
-                        </svg>
+                        <i class="bi bi-calendar3 nav-icon"></i>
                         <span class="nav-text">Employee Task</span>
                     </a>
                 </div>
 
                 <div class="nav-item">
                     <a href="stats.php" class="nav-link active">
-                        <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z"/>
-                        </svg>
+                        <i class="bi bi-bar-chart-fill nav-icon"></i>
                         <span class="nav-text">Performance Stats</span>
                     </a>
                 </div>
 
                 <div class="nav-item">
                     <a href="report.php" class="nav-link">
-                        <svg class="nav-icon" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd"/>
-                        </svg>
+                        <i class="bi bi-file-earmark-text-fill nav-icon"></i>
                         <span class="nav-text">Employee Report</span>
                     </a>
                 </div>
